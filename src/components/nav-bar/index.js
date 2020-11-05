ded5af8c33a7a406232e808799fb2041ea9699f8
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-11-06 00:10:45 +0530

    Remove navbar

--- src/components/nav-bar/index.js
+++ src/components/nav-bar/index.js
@@ -88,7 +88,7 @@ class NavBar extends Component {
     return (
       <>
         <div className='top-nav'>
-          <button className="btn-action btn btn-dark d-none d-sm-block d-md-block d-lg-block d-xl-block" onClick={ this.props.onBack }><i className="fa fa-arrow-left"/></button>
+          {/* <button className="btn-action btn btn-dark d-none d-sm-block d-md-block d-lg-block d-xl-block" onClick={ this.props.onBack }><i className="fa fa-arrow-left"/></button>
           <button className="btn-action btn btn-dark d-none d-sm-block d-md-block d-lg-block d-xl-block" onClick={ this.props.onForward }><i className="fa fa-arrow-right"/></button>
           <button className="btn-action btn btn-dark" onClick={ this.props.onReload }><i className="fa fa-refresh"/></button>
           <input
@@ -102,7 +102,7 @@ class NavBar extends Component {
             onFocus={ this.onFocus }
           />
           <button className="btn-action btn btn-danger btn-go" onClick={ () => this.props.onUrl('') }><i className='fa fa-times'/></button>
-          { this.renderSettings() }
+          { this.renderSettings() } */}
         </div>
       </>
     );
